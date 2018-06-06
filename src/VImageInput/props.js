6d776d174875a8656044ac8408eea2799f1cc886
Sergej Sintschilin <seregpie@gmail.com>
2018-06-06 15:42:23 +0200

    +

--- src/VImageInput/props.js
+++ src/VImageInput/props.js
@@ -11,8 +11,55 @@ export default {
 		default: 256,
 	},
 
+	minImageWidth: {
+		type: Number,
+		default: 0,
+	},
+
+	maxImageWidth: {
+		type: Number,
+		default: Infinity,
+	},
+
+	minImageHeight: {
+		type: Number,
+		default: 0,
+	},
+
+	maxImageHeight: {
+		type: Number,
+		default: Infinity,
+	},
+
 	clearable: {
 		type: Boolean,
 		default: false,
 	},
+
+	croppable: {
+		type: Boolean,
+		default: false,
+	},
+
+	movable: {
+		type: Boolean,
+		default: false,
+	},
+
+	flippable: {
+		type: Boolean,
+		default: false,
+	},
+
+	rotatable: {
+		type: Boolean,
+		default: false,
+	},
+
+	zoomable: {
+		type: Boolean,
+		default: false,
+	},
+
+	// contain
 };

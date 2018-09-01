991557a055f1aad76b583c03f438c6f544798608
Sergej Sintschilin <seregpie@gmail.com>
2018-09-01 19:16:10 +0200

    remove text props from demo

--- demo/script.js
+++ demo/script.js
@@ -9,11 +9,7 @@
 				dark: false,
 				dialog: false,
 				drawer: true,
-				flipHorizontallyText: VuetifyImageInput.props.flipHorizontallyText.default,
-				flipVerticallyText: VuetifyImageInput.props.flipVerticallyText.default,
 				hideActions: VuetifyImageInput.props.hideActions.default,
-				rotateClockwiseText: VuetifyImageInput.props.rotateClockwiseText.default,
-				rotateCounterclockwiseText: VuetifyImageInput.props.rotateCounterclockwiseText.default,
 				value: null,
 			};
 		},

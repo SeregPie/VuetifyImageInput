ddfeddfaed546ee1c1d8d7566a07e25d0babdedf
Sergej Sintschilin <seregpie@gmail.com>
2018-09-01 12:45:24 +0200

    playground with properties

--- demo/script.js
+++ demo/script.js
@@ -5,7 +5,12 @@
 
 		data: function() {
 			return {
+				clearable: true,
+				dark: false,
 				dialog: false,
+				drawer: true,
+				notFlippable: VuetifyImageInput.props.notFlippable.default,
+				notRotatable: VuetifyImageInput.props.notRotatable.default,
 				value: null,
 			};
 		},

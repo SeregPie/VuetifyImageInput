5a5e8ec2d752a805abe1c4e180fd42fe37ec10a2
Sergej Sintschilin <seregpie@gmail.com>
2018-09-07 21:57:53 +0200

    replace uglify with minify

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import buble from 'rollup-plugin-buble';
+import minify from 'rollup-plugin-babel-minify';
 import path from 'path';
 import resolve from '@seregpie/rollup-plugin-resolve';
-import {uglify} from 'rollup-plugin-uglify';
 
 import {main} from './package.json';
 
@@ -21,6 +21,6 @@ export default {
 	plugins: [
 		resolve(),
 		buble(),
-		uglify(),
+		minify(),
 	],
 };

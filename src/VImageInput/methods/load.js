df3b4a6e9bee511ebf5cc04c7431118645dcaadb
Sergej Sintschilin <seregpie@gmail.com>
2019-05-23 16:59:35 +0200

    update load function

--- src/VImageInput/methods/load.js
+++ src/VImageInput/methods/load.js
@@ -2,21 +2,17 @@ export default function(data) {
 	if ((this.imageData || data) && this.imageData !== data) {
 		let image = new Image();
 		image.addEventListener('load', () => {
-			let width = image.naturalWidth || image.width;
-			let height = image.naturalHeight || image.height;
-			if (width && height) {
-				this.internalImageData = data;
-				this.internalImageWidth = width;
-				this.internalImageHeight = height;
-				this.flippedHorizontally = false;
-				this.flippedVertically = false;
-				this.rotated = false;
-				this.scaling = 0;
-				this.dirtyOriginLeft = 1/2;
-				this.dirtyOriginTop = 1/2;
-			} else {
-				this.internalImageData = null;
-			}
+			let width = image.naturalWidth;
+			let height = image.naturalHeight;
+			this.internalImageData = (width && height) ? data : null;
+			this.internalImageWidth = width;
+			this.internalImageHeight = height;
+			this.flippedHorizontally = false;
+			this.flippedVertically = false;
+			this.rotated = false;
+			this.scaling = 0;
+			this.dirtyOriginLeft = 1/2;
+			this.dirtyOriginTop = 1/2;
 		});
 		image.src = data;
 	}

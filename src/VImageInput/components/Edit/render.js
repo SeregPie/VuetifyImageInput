04a270709f1c2b871abf46d32a0796b6c133358d
Sergej Sintschilin <seregpie@gmail.com>
2020-01-30 16:32:35 +0100

    +

--- src/VImageInput/components/Edit/render.js
+++ src/VImageInput/components/Edit/render.js
@@ -1,5 +1,22 @@
 import MyActionLayer from './components/ActionLayer';
 
+import background from './constants/background';
+
 export default function(h) {
-	return h(MyActionLayer);
+	return h(
+		'div',
+		{
+			style: {
+				bottom: 0,
+				background,
+				left: 0,
+				position: 'absolute',
+				right: 0,
+				top: 0,
+			},
+		},
+		[
+			h(MyActionLayer),
+		],
+	);
 }

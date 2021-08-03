84ebf48482c6cb7c06ac60a7c373b3380c4f0051
Sergej Sintschilin <seregpie@gmail.com>
2021-08-03 15:57:48 +0200

    +

--- src/components/VImageInput.spec.js
+++ src/components/VImageInput.spec.js
@@ -3,10 +3,40 @@ import {nextTick} from 'vue';
 
 import VImageInput from './VImageInput';
 
-describe('VImageInput', () => {
-	test('1', async () => {
+describe('wyzpweob', () => {
+	test('ysrqprjs', async () => {
+		let wrapper = mount(VImageInput);
+		await nextTick();
+		expect(wrapper.emitted('update:modelValue')).toHaveLength(1);
+		expect(wrapper.emitted('update:modelValue')[0][0]).toBeNull();
+		await wrapper.setProps({
+			modelValue: '',
+		});
+		expect(wrapper.emitted('update:modelValue')).toHaveLength(2);
+		expect(wrapper.emitted('update:modelValue')[1][0]).toBeNull();
+
+	});
+	test('qjeuvnlo', async () => {
+		let dataURL;
 		let wrapper = mount(VImageInput, {
 			props: {}
+		});
+		await nextTick();
+		expect(wrapper.emitted('update:modelValue')).toHaveLength(1);
+		expect(wrapper.emitted('update:modelValue')[0][0]).toBeNull();
+		await wrapper.vm.load(validFile);
+		expect(wrapper.emitted()['load']).toHaveLength(1);
+		expect(wrapper.emitted()['load'][0]).toEqual([]);
+		await wrapper.vm.load(invalidFile);
+		expect(wrapper.emitted()['error']).toHaveLength(2);
+		expect(wrapper.emitted()['error'][0]).toEqual([]);
+	});
+	test('jndtyziu', async () => {
+		let dataURL;
+		let wrapper = mount(VImageInput, {
+			props: {
+				modelValue: dataURL,
+			}
 		});
 		await wrapper.vm.load(validFile);
 		expect(wrapper.emitted()).toHaveProperty('load');

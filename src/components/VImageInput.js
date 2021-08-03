f255514a4bd2c063a3422ff2af67c6293debae08
Sergej Sintschilin <seregpie@gmail.com>
2021-08-03 12:32:19 +0200

    +

---
+++ src/components/VImageInput.js
@@ -0,0 +1,20 @@
+import {
+	defineComponent,
+	h,
+} from 'vue';
+
+export default defineComponent({
+	name: 'VImageInput',
+	props: {
+
+	},
+	setup(props, {
+		emit,
+		expose,
+		slots,
+	}) {
+		return (() => {
+			return h('div');
+		});
+	},
+});
---
+++ src/components/VImageInput.spec.js
@@ -0,0 +1,11 @@
+import {mount} from '@vue/test-utils';
+
+import VImageInput from './VImageInput';
+
+describe('VImageInput', () => {
+	test('1', () => {
+		let wrapper = mount(VImageInput, {
+			props: {}
+		});
+	});
+});

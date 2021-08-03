26423d887075b46416fea5087ea084bf13409577
Sergej Sintschilin <seregpie@gmail.com>
2021-08-03 14:50:31 +0200

    +

--- src/components/VImageInput.js
+++ src/components/VImageInput.js
@@ -6,13 +6,19 @@ import {
 export default defineComponent({
 	name: 'VImageInput',
 	props: {
-
+		modelValue: String,
 	},
 	setup(props, {
 		emit,
 		expose,
 		slots,
 	}) {
+		let load = (() => {
+
+		});
+		expose({
+			load,
+		});
 		return (() => {
 			return h('div');
 		});
--- src/components/VImageInput.spec.js
+++ src/components/VImageInput.spec.js
@@ -1,11 +1,19 @@
 import {mount} from '@vue/test-utils';
+import {nextTick} from 'vue';
 
 import VImageInput from './VImageInput';
 
 describe('VImageInput', () => {
-	test('1', () => {
+	test('1', async () => {
 		let wrapper = mount(VImageInput, {
 			props: {}
 		});
+		await wrapper.vm.load(validFile);
+		expect(wrapper.emitted()).toHaveProperty('load');
+		expect(wrapper.emitted()['load']).toHaveLength(2);
+		expect(wrapper.emitted()['load'][0]).toEqual([]);
+		await wrapper.vm.load(invalidFile);
+		expect(wrapper.emitted()['error']).toHaveLength(2);
+		expect(wrapper.emitted()['error'][0]).toEqual([]);
 	});
 });

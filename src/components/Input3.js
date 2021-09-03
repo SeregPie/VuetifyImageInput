78ec119586badae8a8b6055e83039468d88aad8c
Sergej Sintschilin <s.sintschilin@itsonix.eu>
2021-09-03 10:48:06 +0200

    +

--- src/components/Input3.js
+++ src/components/Input3.js
@@ -1,30 +1,61 @@
+import {
+	computed,
+	defineComponent,
+	h,
+	shallowRef,
+	watch,
+	watchEffect,
+} from 'vue';
+
 export default defineComponent({
 	name: 'VImageInput',
 	props: {
+		max: {
+			type: Number,
+			default: 100,
+		},
+		min: {
+			type: Number,
+			default: 0,
+		},
 		modelValue: {
-			type: String,
-			default: null,
+			type: Number,
+			default: 0,
 		},
 	},
-	setup(props, {emit}) {
-		let chxpqwgbRef = ref(pros.modelValue);
-		let atgczxkfRef = computed(() => {
-			let value = chxpqwgbRef.value;
-			let settings = pros.settings;
-			return value * settings;
-		});
+	setup(props, {
+		emit,
+		expose,
+	}) {
+		let chxpqwgbRef = shallowRef(props.modelValue);
 		watch(
-			atgczxkfRef,
+			chxpqwgbRef,
 			(value) => {
 				emit('update:modelValue', value);
 			},
 		);
+		let orwtlcwoRef = shallowRef(props.modelValue);
 		watch(
 			() => props.modelValue,
 			(value) => {
-				chxpqwgbRef.value = value;
+				orwtlcwoRef.value = value;
 			},
 		);
+		let atgczxkfRef = computed(() => {
+			let n = orwtlcwoRef.value;
+			let {
+				max,
+				min,
+			} = props;
+			return Math.min(Math.max(n, min), max);
+		});
+		watchEffect(() => {
+			chxpqwgbRef.value = atgczxkfRef.value;
+		});
+		let setValue = ((value) => {
+			orwtlcwoRef.value = value;
+		});
+		expose({setValue});
 		return (() => h('div'));
 	},
 });
